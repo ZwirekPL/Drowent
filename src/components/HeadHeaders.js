1b8438dbf17f1bf393075988bca9adb8565d502d
Zwirek_Dev <glanzgarage@gmail.com>
2022-10-09 14:44:14 +0200

    fixed path, fighting with navbar

--- src/components/HeadHeaders.js
+++ src/components/HeadHeaders.js
@@ -9,11 +9,11 @@ const HeadHeaders = () => {
                 <div class="swiper-slide">
                     <div class="container-general">
                         <div class="gallery-wrap wrap-effect-1">
-                            <div class="item"><span>Klimatyzacje</span><button className="pricing__btn">poproś o wycenę</button></div>
-                            <div class="item"><span>Wentylacje</span><button className="pricing__btn">poproś o wycenę</button></div>
-                            <div class="item"><span>Pompy Ciepła</span><button className="pricing__btn">poproś o wycenę</button></div>
-                            <div class="item"><span>Rekuperacja</span><button className="pricing__btn">poproś o wycenę</button></div>
-                            <div class="item"><span>ppoż</span><button className="pricing__btn">poproś o wycenę</button></div>
+                            <div class="item"><span>Klimatyzacje</span></div>
+                            <div class="item"><span>Wentylacje</span></div>
+                            <div class="item"><span>Pompy Ciepła</span></div>
+                            <div class="item"><span>Rekuperacja</span></div>
+                            <div class="item"><span>ppoż</span></div>
                         </div>
                     </div>
                 </div>
