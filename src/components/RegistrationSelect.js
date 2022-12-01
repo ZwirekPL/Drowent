d25e23ec668cc0baf43e0f4c1f91a3927c011409
Zwirek_Dev <glanzgarage@gmail.com>
2022-12-01 16:46:58 +0100

    buttons of registrationselect working properly

--- src/components/RegistrationSelect.js
+++ src/components/RegistrationSelect.js
@@ -8,32 +8,35 @@ import RegistrationCompany from "./RegistrationCompany";
 
 
 const RegistrationSelect = () => {
-    const [activeCustomer, setActiveCustomer] = useState("false");
-    const [activeCompany, setActiveCompany] = useState("false");
+    const [isCloseCustomer, setIsCloseCustomer] = useState(true);
+    const [isCloseCompany, setIsCloseCompany] = useState(true);
 
-    const [anyActive, setAnyActive] = useState("false")
+    
 
-    const activeChecker = () =>{
-        if(activeCustomer === "true")
-        {setAnyActive(!anyActive);
-        console.log(anyActive)}
-    }
+   
     const handleCustomer = () =>{
-        setActiveCustomer(!activeCustomer);
-    }
+        setIsCloseCustomer(!isCloseCustomer);
+        setIsCloseCompany(true);       
+        // console.log(`customer ${isCloseCustomer}`);
+        // console.log(`company ${isCloseCompany}`);
+    };
+
     const handleCompany = () =>{
-        setActiveCompany(!activeCompany);
-    }
+        setIsCloseCompany(!isCloseCompany);
+        setIsCloseCustomer(true);        
+        // console.log(`customer ${isCloseCustomer}`);
+        // console.log(`company ${isCloseCompany}`);
+    };
     return (
         <div className="registrationselect" >
-            <div className={activeCustomer ? "registrationselect__background" : "registrationselect__background-active" }>
-                <div className={activeCustomer ? "registrationselect__btns" : "registrationselect__btns-on"}>
-                    <button onClick={handleCustomer} className={activeCustomer ? "registrationselect__customer-active" : "registrationselect__customer-disable" }><p>Konto Prywatne</p></button>
+            <div className={!isCloseCustomer ^ !isCloseCompany ? "registrationselect__background-active" : "registrationselect__background" }>
+                <div className={!isCloseCustomer ^ !isCloseCompany ? "registrationselect__btns-on" : "registrationselect__btns"}>
+                    <button onClick={handleCustomer} className="registrationselect__customer"><p>Konto Prywatne</p></button>
                     <button onClick={handleCompany} className="registrationselect__company"><p>Konto Firmowe</p></button>
                 </div>
                 <div className="registrationselect__forms">
-                    <div className={!activeCustomer ? "customer__active" : "customer__disable"}><RegistrationCustomer/></div>
-                    <div className={!activeCompany ? "company__active" : "company__disable"}><RegistrationCompany/></div>
+                    <div className={!isCloseCustomer ? "customer__active" : "customer__disable"}><RegistrationCustomer/></div>
+                    <div className={!isCloseCompany ? "company__active": "company__disable"}><RegistrationCompany/></div>
                 </div>
             </div>
         </div>
