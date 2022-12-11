d4caed935398a86403edccc0ee43d6502c8397f5
Zwirek_Dev <glanzgarage@gmail.com>
2022-12-11 09:49:08 +0100

    add contact form from hook form. add h3 and input form mixins. styling contact form.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,9 @@ const Contact = () => {
             <div className="contact__tel"><a href="tel:+48123456789"><i class="icon-phone"></i>123456789</a></div>
             <div className="contact__mail"><a href="mailto: biuro@drowent.pl"><i class="icon-mail-squared"></i>biuro@drowent.pl</a></div>
             <div className="contact__address"><p><i class="icon-home"></i>Mieczysława Pożaryskiego 19, 04-703 Warszawa</p></div>
+            <span className="nip">nip</span>
+            <span className="regon">regon</span>
+            <span className="Krs">Krs</span>
         </div>
             <div className="contact__right"><ContactForm/></div>       
         </div>
--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,27 +1,52 @@
 
+import { useForm } from 'react-hook-form';
 
 import "../style/sass/contactForm.sass";
 
 const ContactForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
     return (
         <div className="contactForm">
             <div className="contactForm__forms">
-                <div className="contactForm__sign">                    
-                    <form action="#">
-                        <h2>Formularz Kontaktowy</h2>
-			            <span>Uzupełnij wszystkie wymagane pola.</span>
-                        <div className="contactForm__input">
-			                <input type="email" placeholder="Email" />
-			                <input type="password" placeholder="Hasło" />
-			                <input type="password" placeholder="Hasło" />
-			                <input type="text" placeholder="Numer NIP" />
-                        </div>
-			            <button className="contactForm__signup"><p>Wyślij</p></button>
-		            </form>                    
-                </div>                    
+                <div className="contactForm__sign">
+                    <h3>Formularz Kontaktowy</h3>
+                    <span>Uzupełnij wszystkie wymagane pola.</span>
+                    <form className="contactForm__form" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="contactForm__input">
+                        <input type="text" placeholder="Imię" {...register("name", {required: true, max: 15, min: 3, pattern: /"[a-zA-Z]"/i})} />
+                        <input type="text" placeholder="Nazwisko" {...register("laName", {required: true, max: 20, min: 1, pattern: /"[a-zA-Z]"/i})} />
+                        <input type="email" placeholder="E-mail" {...register("email", {required: true, pattern: /"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/i})} />
+                        <input type="tel" placeholder="Telefon" {...register("phone", {required: true, max: 9, min: 9, pattern: /"[0-9]{3}"/i})} />
+                        <select {...register("guest", { required: true })}>
+                            <option value="" selected disabled hidden>Wybierz</option>
+                            <option value="Klient Indywidualny">Klient Indywidualny</option>
+                            <option value=" Firma"> Firma</option>
+                        </select>
+                        <select {...register("line", { required: true })}>
+                            <option value="" selected disabled hidden>wybierz temat</option>
+                            <option value="Klimatyzacja">Klimatyzacja</option>
+                            <option value="Wentylacja">Wentylacja</option>
+                            <option value="Pompy Ciepła">Pompy Ciepła</option>
+                            <option value="Rekuperacja">Rekuperacja</option>
+                            <option value="ppoż">ppoż</option>
+                        </select>
+                        <select {...register("subject", { required: true })}>
+                            <option value="" selected disabled hidden>wybierz wątek</option>
+                            <option value="wycena">wycena</option>
+                            <option value="montaż">montaż</option>
+                            <option value="zakup">zakup</option>
+                            <option value="serwis">serwis</option>
+                            <option value="współpraca">współpraca</option>
+                        </select>
+                        <textarea placeholder="Wpisz treść zapytania" {...register("text", {required: true, min: 10})} />
+                        <input type="submit" />
+                    </div>
+                    </form>
+                </div>
             </div>
         </div>
-    );
+        );
 }
-  
-  export default ContactForm;
+export default ContactForm;
