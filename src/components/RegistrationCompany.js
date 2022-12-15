0f9e8b41ea6fe6a1785d4b9f1063a57b03b313f7
Zwirek_Dev <glanzgarage@gmail.com>
2022-12-15 13:23:39 +0100

    improvement of registration forms

--- src/components/RegistrationCompany.js
+++ src/components/RegistrationCompany.js
@@ -1,22 +1,26 @@
-
+import { useForm } from 'react-hook-form';
 
 import "../style/sass/RegistrationCompany.sass";
 
 const RegistrationCompany = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+  
     return (
         <div className="registrationcompany">
             <div className="registrationcompany__forms">
                 <div className="registrationcompany__sign">                    
-                    <form action="#">
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <h2>Zarejestruj</h2>
 			            <span>Konto Firmowe</span>
                         <div className="registrationcompany__input">
-			                <input type="email" placeholder="Email" />
-			                <input type="password" placeholder="Hasło" />
-			                <input type="password" placeholder="Hasło" />
-			                <input type="text" placeholder="Numer NIP" />
+                            <input type="email" placeholder="E-mail" {...register("email", {required: true, pattern: /"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/i})} />
+                            <input type="password" placeholder="Hasło" {...register("Password", {required: true})} />
+			                <input type="password" placeholder="Powtórz Hasło" {...register("Password", {required: true})} />
+			                <input type="text" placeholder="Numer NIP " {...register("Numer NIP ", {required: true, pattern: /^((\d{3}[- ]\d{3}[- ]\d{2}[- ]\d{2})|(\d{3}[- ]\d{2}[- ]\d{2}[- ]\d{3}))$/i})} />
                         </div>
-			            <button className="registrationcompany__signup"><p>Wyślij</p></button>
+			            <input type="submit" />
 		            </form>                    
                 </div>                    
             </div>
--- src/components/RegistrationCustomer.js
+++ src/components/RegistrationCustomer.js
@@ -1,21 +1,25 @@
-
+import { useForm } from 'react-hook-form';
 
 import "../style/sass/RegistrationCustomer.sass";
 
 const RegistrationCustomer = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+  
     return (
         <div className="registrationCustomer">
             <div className="registrationCustomer__forms">
                 <div className="registrationCustomer__sign">                    
-                    <form action="#">
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <h2>Zarejestruj</h2>
 			            <span>Konto Prywatne</span>
                         <div className="registrationCustomer__input">
-			                <input type="email" placeholder="Email" />
-			                <input type="password" placeholder="Hasło" />
-			                <input type="password" placeholder="Hasło" />
+                            <input type="email" placeholder="E-mail" {...register("email", {required: true, pattern: /"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/i})} />
+                            <input type="password" placeholder="Hasło" {...register("Password", {required: true})} />
+			                <input type="password" placeholder="Powtórz Hasło" {...register("Password", {required: true})} />
                         </div>
-			            <button className="registrationCustomer__signup"><p>Wyślij</p></button>
+                        <input type="submit" />
 		            </form>                    
                 </div>                    
             </div>
@@ -24,3 +28,5 @@ const RegistrationCustomer = () => {
 }
   
   export default RegistrationCustomer;
+
+
