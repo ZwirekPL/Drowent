ebacb20832de62a63d74c50659107f0db98e5a80
Zwirek_Dev <glanzgarage@gmail.com>
2023-01-02 19:05:25 +0100

    MQ for contact

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,18 +1,36 @@
 import HeaderSections from "./HeaderSections";
+import HeadFooter from "./HeadFooter";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import Contact from "./contact";
 import Hamburger from "./hamburger";
+import useWindowDimensions from "./setWindowSize";
 
 import "../style/sass/ContactSection.sass";
 
 function ContactSection() {
+  const { width } = useWindowDimensions();
+  if (width > 768) {
+    return (
+      <div className="header">
+        <header>
+          <HeaderSections />
+          <Nav />
+        </header>
+        <main>
+          <Contact />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    );
+  }
   return (
-    <div className="contactSection">
+    <div className="header">
       <header>
-        <HeaderSections />
-        <Nav />
         <Hamburger />
+        <HeadFooter />
       </header>
       <main>
         <Contact />
