fcf1421f111e9286373521be9c47cc69052e31a0
Zwirek_Dev <glanzgarage@gmail.com>
2023-01-05 15:19:46 +0100

    make sitemap and footer responive for phone

--- src/components/ventilationSection.js
+++ src/components/ventilationSection.js
@@ -1,19 +1,37 @@
 import HeaderSections from "./HeaderSections";
 import Nav from "./Nav";
-// import HeadFooter from "./HeadFooter";
+import HeadFooter from "./HeadFooter";
 import Footer from "./Footer";
 import VentilationComponent from "./ventilationComponent";
 import CarouselOpinion from "./CarouselOpinion";
 import "../style/sass/ventilationSection.sass";
 import Hamburger from "./hamburger";
+import useWindowDimensions from "./setWindowSize";
 
 function VentilationSection() {
+  const { width } = useWindowDimensions();
+  if (width > 768) {
+    return (
+      <div className="ventilation">
+        <header>
+          <HeaderSections />
+          <Nav />
+        </header>
+        <main>
+          <VentilationComponent />
+          <CarouselOpinion />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    );
+  }
   return (
     <div className="ventilation">
       <header>
-        <HeaderSections />
-        <Nav />
         <Hamburger />
+        <HeadFooter />
       </header>
       <main>
         <VentilationComponent />
@@ -25,5 +43,4 @@ function VentilationSection() {
     </div>
   );
 }
-
 export default VentilationSection;
