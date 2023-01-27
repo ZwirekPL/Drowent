f23a32e2c2fce3231a3ca61225029e3f92c96d24
Zwirek_Dev <glanzgarage@gmail.com>
2023-01-27 17:09:08 +0100

    add validation to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,66 @@
-import ShowHidePassword from "./showHidePassword";
+import { useState } from "react";
 
 import "../style/sass/Login.sass";
 
+const validate = (loginData) => {
+  const errorsArray = [];
+  // console.log(loginData.message.length);
+  // console.log(loginData.message);
+  console.log(loginData.email);
+  // console.log(loginData.name.length);
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(loginData.email)) {
+    errorsArray.push("E-mail jest niepoprawny");
+  }
+  if (loginData.password.length < 8) {
+    errorsArray.push("Hasło musi posiadać min. 8 znaków.");
+  }
+  // walidacja hasła pod numer?
+  if (errorsArray.length !== 0) {
+    console.log(errorsArray);
+    return errorsArray;
+  } else {
+    return false;
+  }
+};
+
 const Login = () => {
+  const loginInit = {
+    email: "",
+    password: "",
+  };
+  const [passIsVisible, setPassVisible] = useState(false);
+  const [loginData, setloginData] = useState({ ...loginInit });
+  const [isLogged, setIsLogged] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = ({ target }) => {
+    setloginData({
+      ...loginData,
+      [target.name]: target.value,
+    });
+  };
+
+  const togglePassword = () => {
+    setPassVisible(!passIsVisible);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const errorMessage = validate(loginData);
+    if (errorMessage) {
+      setError(errorMessage);
+      return console.log("error", errorMessage[1]);
+    } else {
+      console.log("form submitted", loginData);
+      // console.log("error", error[0], error);
+      setIsLogged(true);
+      setError(null);
+      setTimeout(function () {
+        setIsLogged(false);
+      }, 2000);
+      setloginData({ ...loginInit });
+    }
+  };
   return (
     <section className="login">
       <div className="login__forms">
@@ -22,16 +80,51 @@ const Login = () => {
             </nav>
             <span>albo użyj email do zalogowania</span>
             <div className="login__input">
-              <input type="email" placeholder="Email" />
-
-              <ShowHidePassword name="Hasło" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+                value={loginData.email}
+                autocomplete="email"
+                required
+              />
+              <div className="formHide">
+                <input
+                  type={!passIsVisible ? "password" : "text"}
+                  name="password"
+                  placeholder={"Hasło"}
+                  onChange={handleChange}
+                  value={loginData.password}
+                  autocomplete="current-password"
+                  id="current-password"
+                  required
+                />
+                <span className="icon" onClick={togglePassword}>
+                  {passIsVisible ? (
+                    <i className="icon-eye"></i>
+                  ) : (
+                    <i className="icon-eye-off"></i>
+                  )}
+                </span>
+              </div>
             </div>
             <button className="login__forgot">
               <p>zapomniałeś hasła ?</p>
             </button>
-            <button className="login__signup">
+            <button className="login__signup" onClick={handleClick}>
               <p>Zaloguj</p>
             </button>
+            {isLogged ? (
+              <div className="isLogged">
+                <p>Zostałeś poprawnie zalogowany</p>
+              </div>
+            ) : null}
+            {error == null ? null : (
+              <div className="errorMessage">
+                <p>{error[0]}</p>
+              </div>
+            )}
           </form>
         </div>
       </div>
